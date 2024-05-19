1410b2c45cd6ff743ec4759254d9a8a6840f0d4a
Krislv4nk <kristinalev4enko89@gmail.com>
2024-05-19 20:10:04 +0200

    config background

--- src/components/Background/Background.styled.jsx
+++ src/components/Background/Background.styled.jsx
@@ -9,15 +9,13 @@ export const VideoWrapper = styled.div`
 `
 
 export const VideoBackground = styled.video`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  min-width: 100%;
-  min-height: 100%;
-  width: auto;
-  height: auto;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
   z-index: -1;
-  transform: translate(-50%, -50%);
 `
 
 
