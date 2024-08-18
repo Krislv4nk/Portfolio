b6466bbc565d395af6caf9a30b70d87073af1eae
Krislv4nk <kristinalev4enko89@gmail.com>
2024-08-18 19:35:26 +0200

    update buttons

--- src/pages/ProjectsPage/ProjectsPage.jsx
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -1,6 +1,5 @@
 
 import { useState } from 'react';
-import Slider from 'react-slick';
 import css from './ProjectsPage.module.css';
 import CamperClubPage from 'pages/CamperClubPage/CamperClubPage';
 import WaterTrackerPage from 'pages/WaterTrackerPage/WaterTrackerPage';
@@ -14,15 +13,15 @@ const ProjectsPage = () => {
     const renderTabContent = () => {
         switch (activeTab) {
             case 'LearnLingo':
-                return <LearnLingoPage className={css.tabContent}/>;
+                return <LearnLingoPage className={css.tabContent} />;
             case 'CamperClub':
-                return <CamperClubPage className={css.tabContent}/>;
+                return <CamperClubPage className={css.tabContent} />;
             case 'WaterTracker':
-                return <WaterTrackerPage className={css.tabContent}/>;
+                return <WaterTrackerPage className={css.tabContent} />;
             case 'PhoneBook':
-                return <PhoneBookPage className={css.tabContent}/>;
+                return <PhoneBookPage className={css.tabContent} />;
             case 'FoodStore':
-                return <FoodStorePage className={css.tabContent}/>;
+                return <FoodStorePage className={css.tabContent} />;
             default:
                 return (
                     <p className={css.typing}>Please select a project to view.</p>
@@ -30,39 +29,58 @@ const ProjectsPage = () => {
         }
     };
 
-    const settings = {
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        afterChange: (current) => setActiveTab(tabs[current].id)
-    };
-
-    const tabs = [
-        { id: 'LearnLingo', label: 'LearnLingo' },
-        { id: 'CamperClub', label: 'CamperClub' },
-        { id: 'WaterTracker', label: 'WaterTracker' },
-        { id: 'PhoneBook', label: 'PhoneBook' },
-        { id: 'FoodStore', label: 'FoodStore' }
-    ];
-
     return (
         <div className={css.wrapper}>
-            <h2 className={css.fifthFrame}>Let me show you what I have created</h2>
-            <Slider {...settings}>
-                {tabs.map(tab => (
-                    <div key={tab.id} className={css.tabSlide}>
-                        <button
-                            className={`${css.item} ${activeTab === tab.id ? css.active : ''}`}
-                            onClick={() => setActiveTab(tab.id)}
-                            title={tab.label}
-                        >
-                            {tab.label}
-                        </button>
-                    </div>
-                ))}
-            </Slider>
+            <h2 className={css.fifthFrame}>
+                Let me show you what I have created
+            </h2>
+            <ul className={css.list}>
+                <li className={css.projectItemLeft}>
+                    <button
+                        className={`${css.item} ${activeTab === 'LearnLingo' ? css.active : ''}`}
+                        onClick={() => setActiveTab('LearnLingo')}
+                        title='LearnLingo'
+                    >
+                        LearnLingo
+                    </button>
+                </li>
+                <li className={css.projectItem}>
+                    <button
+                        className={`${css.item} ${activeTab === 'CamperClub' ? css.active : ''}`}
+                        onClick={() => setActiveTab('CamperClub')}
+                        title='CamperClub'
+                    >
+                        CamperClub
+                    </button>
+                </li>
+                <li className={css.projectItemLeft}>
+                    <button
+                        className={`${css.item} ${activeTab === 'WaterTracker' ? css.active : ''}`}
+                        onClick={() => setActiveTab('WaterTracker')}
+                        title='WaterTracker'
+                    >
+                        Water Tracker
+                    </button>
+                </li>
+                <li className={css.projectItem}>
+                    <button
+                        className={`${css.item} ${activeTab === 'PhoneBook' ? css.active : ''}`}
+                        onClick={() => setActiveTab('PhoneBook')}
+                        title='PhoneBook'
+                    >
+                        Phone Book
+                    </button>
+                </li>
+                <li className={css.projectItemLeft}>
+                    <button
+                        className={`${css.item} ${activeTab === 'FoodStore' ? css.active : ''}`}
+                        onClick={() => setActiveTab('FoodStore')}
+                        title='FoodStore'
+                    >
+                        Food Store
+                    </button>
+                </li>
+            </ul>
             <div className={css.tabContent}>
                 {renderTabContent()}
             </div>
@@ -71,4 +89,6 @@ const ProjectsPage = () => {
 };
 
 export default ProjectsPage;
+
+
 
