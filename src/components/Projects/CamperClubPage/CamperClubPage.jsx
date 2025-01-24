afbac50d160badbbe137a7c50a77dd99fa3d9f66
Krislv4nk <kristinalev4enko89@gmail.com>
2025-01-24 18:17:34 +0100

    new design project card

--- src/components/Projects/CamperClubPage/CamperClubPage.jsx
+++ src/components/Projects/CamperClubPage/CamperClubPage.jsx
@@ -1,5 +1,5 @@
 
-
+import { useState } from 'react';
 import camper from '../../../assets/img/camper.png';
 import camperA from '../../../assets/img/camper-1.png';
 import css from './CamperClubPage.module.css';
@@ -9,12 +9,28 @@ import { motion } from 'framer-motion';
 import { ScrollableComponent } from '../../ScrollableComponent/ScrollableComponent';
 
 const CamperClubPage = () => {
-
+  const [isExpanded, setIsExpanded] = useState(false);
+  
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
   return (
-    <div className={css.wrapperRight}>
+    <div className={`${css.wrapperRight} ${isExpanded ? css.expanded : ''}`}>
       <div className={css.titleContainer}>
-        <div>
-      <h2 className={css.title}>CamperClub</h2>
+        <h2 className={css.title}>CamperClub</h2>
+        <div className={css.container}>
+  <div className={css.listWrapper}>
+       <motion.img className={css.imgCamper} src={camper} alt="camper" whileHover={{
+    scale: 1.5, 
+    transition: { duration: 0.5, ease: 'easeInOut' }, 
+  }}
+  initial={{ scale: 1 }}/>
+        <motion.img  className={css.imgCamper} src={camperA} alt="camper" whileHover={{
+    scale: 1.5, 
+    transition: { duration: 0.5, ease: 'easeInOut' }, 
+  }}
+  initial={{ scale: 1 }}/>
+        </div>
       <ul className={css.linksWrapper}>
            <li className={css.linksItem}> <svg className={css.repoLinkIcon}>
           <use href={`${icons}#icon-link`}></use>
@@ -28,54 +44,46 @@ const CamperClubPage = () => {
           title="View Front-End Code on GitHub">Front-End GitHub</Link></li>
       </ul>
 </div>
-      <div className={css.listWrapper}>
-       <motion.img className={css.imgCamper} src={camper} alt="camper" whileHover={{
-    scale: 1.5, 
-    transition: { duration: 0.5, ease: 'easeInOut' }, 
-  }}
-  initial={{ scale: 1 }}/>
-        <motion.img  className={css.imgCamper} src={camperA} alt="camper" whileHover={{
-    scale: 1.5, 
-    transition: { duration: 0.5, ease: 'easeInOut' }, 
-  }}
-  initial={{ scale: 1 }}/>
-        </div>
-        </div>
-      <ScrollableComponent>
-        <div className={css.descriptionWrapper}>
-<h4 className={css.descriptionTitle}>Individual Project</h4>
-          <p className={css.description}>Introducing my latest venture: the premier individual project following my
-            graduation from IT school. Meet the
-            React-powered camper rental website, complete with Redux integration! Immerse yourself in a seamless
-            browsing experience
-            as you explore our collection of available campers. From detailed descriptions to captivating visuals, each
-            camper page
-            offers a glimpse into your next adventure. Plus, with the ability to add your favorite finds to a
-            personalized favorites
-            list, planning your dream getaway has never been easier. Join us as we redefine the way adventurers connect
-            with their
-            ideal camper experience. Welcome to a world of exploration, all at your fingertips.</p>
-<h4 className={css.descriptionTitle}>Key Features:</h4>
-        <ul className={css.featuresList}>
-           <li className={css.featuresItem}>  Browse Campers: Users can browse a list of available campers with images, prices, and other details.</li>
-            <li className={css.featuresItem}> Camper Details: Each camper has a dedicated page with detailed information, including description,
-            location, rating,
-            reviews, and specifications.</li>
-            <li className={css.featuresItem}> Add to Favorites: Users can add campers to their favorites list and remove them from it.
-            </li >
+      </div>
+       <button className={css.toggleExpand} onClick={toggleExpand} type='button'>
+          {isExpanded ? 'Show less' : 'Read more'}
+      </button>
+      {isExpanded && (
+        <ScrollableComponent>
+          <div className={css.descriptionWrapper}>
+            <h4 className={css.descriptionTitle}>Individual Project</h4>
+            <p className={css.description}> Meet the
+              React-powered camper rental website, complete with Redux integration! Immerse yourself in a seamless
+              browsing experience
+              as you explore our collection of available campers. From detailed descriptions to captivating visuals, each
+              camper page
+              offers a glimpse into your next adventure. Plus, with the ability to add your favorite finds to a
+              personalized favorites
+              list, planning your dream getaway has never been easier. Join us as we redefine the way adventurers connect
+              with their
+              ideal camper experience. Welcome to a world of exploration, all at your fingertips.</p>
+            <h4 className={css.descriptionTitle}>Key Features:</h4>
+            <ul className={css.featuresList}>
+              <li className={css.featuresItem}>  Browse Campers: Users can browse a list of available campers with images, prices, and other details.</li>
+              <li className={css.featuresItem}> Camper Details: Each camper has a dedicated page with detailed information, including description,
+                location, rating,
+                reviews, and specifications.</li>
+              <li className={css.featuresItem}> Add to Favorites: Users can add campers to their favorites list and remove them from it.
+              </li >
             </ul >
-          <h4 className={css.descriptionTitle}>Tools: </h4>
+            <h4 className={css.descriptionTitle}>Tools: </h4>
             <ul className={css.toolsList}>
-                <li className={css.tool}>React,</li>
-                <li className={css.tool}>Redux,</li>
-                <li className={css.tool}>MUi (Material-UI),</li>
-                <li className={css.tool}>Axios,</li>
-                <li className={css.tool}>Formik,</li>
-                <li className={css.tool}>Yup;</li>
-              </ul>
-          <p className={css.description}>Duration: 3 weeks</p>
+              <li className={css.tool}>React,</li>
+              <li className={css.tool}>Redux,</li>
+              <li className={css.tool}>MUi (Material-UI),</li>
+              <li className={css.tool}>Axios,</li>
+              <li className={css.tool}>Formik,</li>
+              <li className={css.tool}>Yup;</li>
+            </ul>
+            <p className={css.description}>Duration: 3 weeks</p>
           </div>
         </ScrollableComponent>
+      )}
     </div>
   );
 };
--- src/pages/ProjectsPage/ProjectsPage.jsx
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -1,51 +1,15 @@
 
-import { useState, useEffect, useRef } from 'react';
+
 import css from './ProjectsPage.module.css';
 import CamperClubPage from 'components/Projects/CamperClubPage/CamperClubPage';
 import WaterTrackerPage from 'components/Projects/WaterTrackerPage/WaterTrackerPage';
 import PhoneBookPage from 'components/Projects/PhoneBookPage/PhoneBookPage';
 import FoodStorePage from 'components/Projects/FoodStorePage/FoodStorePage';
 import LearnLingoPage from 'components/Projects/LearnLingoPage/LearnLingoPage';
-import symbol from '../../assets/symbol.svg';
+
 
 const ProjectsPage = () => {
-    const [activeTab, setActiveTab] = useState(null);
-
-    const iconRefs = useRef([]); 
-
-    useEffect(() => {
-        iconRefs.current.forEach(icon => {
-            if (icon) {
-                const paths = icon.querySelectorAll('path');
-                paths.forEach(path => {
-                    const length = path.getTotalLength();
-                    path.style.strokeDasharray = length;
-                    path.style.strokeDashoffset = length;
-                    path.style.transition = 'stroke-dashoffset 2s ease-in-out';
-                    path.style.strokeDashoffset = '0';
-                });
-            }
-        });
-    }, [activeTab]);
-
-    const renderTabContent = () => {
-        switch (activeTab) {
-            case 'LearnLingo':
-                return <LearnLingoPage className={css.tabContent} />;
-            case 'CamperClub':
-                return <CamperClubPage className={css.tabContent} />;
-            case 'WaterTracker':
-                return <WaterTrackerPage className={css.tabContent} />;
-            case 'PhoneBook':
-                return <PhoneBookPage className={css.tabContent} />;
-            case 'FoodStore':
-                return <FoodStorePage className={css.tabContent} />;
-            default:
-                return (
-                    <p className={css.typing}>Please select a project to view.</p>
-                );
-        }
-    };
+    
 
     return (
         <div className={css.wrapper}>
@@ -53,34 +17,13 @@ const ProjectsPage = () => {
                 Let me show you what I have created
             </h3>
             <ul className={css.list}>
-                {[
-                    { name: 'LearnLingo', iconId: 'icon-Vector-1' },
-                    { name: 'CamperClub', iconId: 'icon-camper' },
-                    { name: 'WaterTracker', iconId: 'icon-Vector-3' },
-                    { name: 'PhoneBook', iconId: 'icon-bi_phone' },
-                    { name: 'FoodStore', iconId: 'icon-Vector-4' },
-                ].map(({ name, iconId }, index) => (
-                    <li
-                        key={name}
-                        className={index % 2 === 0 ? css.projectItemLeft : css.projectItem}
-                    >
-                        <button
-                            className={`${css.item} ${activeTab === name ? css.active : ''}`}
-                            onClick={() => setActiveTab(name)}
-                            title={name}
-                        ><svg
-                                className={css.icon}
-                                ref={el => (iconRefs.current[index] = el)}
-                            >
-                                <use xlinkHref={`${symbol}#${iconId}`}></use>
-                            </svg>
-                            <span className={css.spanName}>{name}</span>
-                        </button>
-                    </li>
-                    
-                ))}
+                <li><LearnLingoPage /></li>
+                <li><CamperClubPage /></li>
+      <li><WaterTrackerPage /></li>
+      <li><PhoneBookPage /></li>
+      <li><FoodStorePage /></li>
                 </ul>
-            <div className={css.tabContent}>{renderTabContent()}</div>
+            
         </div>
     );
 };
