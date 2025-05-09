022a58aafe07102848a577afcb4a9f996bcc14cd
Krislv4nk <kristinalev4enko89@gmail.com>
2025-05-09 05:11:11 +0200

    carousel styles

--- src/components/Projects/ProjectsSection/ProjectsSection.jsx
+++ src/components/Projects/ProjectsSection/ProjectsSection.jsx
@@ -14,10 +14,17 @@ const ProjectsPage = () => {
     return (
         <div className={css.wrapper}>
             <h3 className={css.title}>Let me show you what I have created</h3>
-            <Carousel
+            <Carousel 
                 className={css.carousel}
+                SxPropsTheme={{
+                    backgroundColor: 'rgba(0, 0, 0, 0.85)',
+                    padding: '60px',
+                    borderRadius: '16px',
+                    Theme: 'dark',
+                    color: 'white',
+                  }}
   autoPlay={false}
-  animation="slide"
+  animation="fade"
   indicators={false}
   navButtonsAlwaysVisible={true}
   navButtonsProps={{
@@ -26,7 +33,10 @@ const ProjectsPage = () => {
       color: 'black',
       borderRadius: 0,
       width: '50px',
-      height: '50px',
+          height: '50px',
+          border: 'red 1px solid',
+          bottom: '0',
+            top: 'unset'
     }
   }}
   swipe={true}
@@ -35,7 +45,7 @@ const ProjectsPage = () => {
                 >
                 
                 {
-                    items.map((item, i) => <Item key={i} item={item} />)
+                    items.map((item, i) => <Item key={i} item={item} className={css.item}/>)
                 }
             </Carousel>
             </div>
@@ -45,18 +55,19 @@ const ProjectsPage = () => {
 }
 const Item = (props) => {
     return (
-        <Paper className={css.paper}>
+        <Paper className={css.paper} elevation={0}>
             <div className={css.imgWrapper}>
                 <img className={css.img} src={props.item.images.first} alt={props.item.name} />
-                </div>
-            <div className={css.imgWrapper}>
-                <img className={css.img} src={props.item.images.second} alt={props.item.name} />
+                
+                {/* <img className={css.img} src={props.item.images.second} alt={props.item.name} /> */}
             </div>
             <div className={css.infoWrapper}>
-            <h2>{props.item.name}</h2>
+                <h2 className={css.infoTitle}>{props.item.name}</h2>
+                <div className={css.linksWrapper}>
             <a href={props.item.links.live} rel="noreferrer" target='_blank' className={css.links}>Live Demo</a>
             <a href={props.item.links.front} rel="noreferrer" target='_blank' className={css.links}>Front-End GitHub</a>
-            <a href={props.item.links.back} rel="noreferrer" target='_blank' className={css.links}>Back-End GitHub</a>
+                    <a href={props.item.links.back} rel="noreferrer" target='_blank' className={css.links}>Back-End GitHub</a>
+                    </div>
             <Button className={css.button}>
                 Show more
                 </Button>
